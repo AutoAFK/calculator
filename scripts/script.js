b2422439397f9c7ed393a6daf82ca3134f2bf5be
AutoAFK <ranhaim85@gmail.com>
2026-03-24 17:46:35 +0200

    Calculator works, needs to get rid of chain op
    Right now chaining opeartors are possible. It
    won't calculate the amount and let you continue
    on the result.

--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,73 @@
-const operations = document.querySelector(".btn-operations");
-const screen = document.querySelector("#screen");
+const app = document.querySelector("#app");
+const screen = document.querySelector(".screen");
 const operationsSigns = "+-*/";
 
-/* =============== NOTE ===============
-iteration is used to see if its the first 
-that the user have clicked on ethier one of the 
-operations.
-
-On the firts round notning will happen and it will
-just display it on the screen. After the first round
-it will evaluate the operation and then enter it to the
-screen.
-*/
 const calculatorObj = {
-    left: 0,
-    right: 0,
-    iteration: 0,
-    currentOperation: '',
+    sum: 0,
     screenText: '',
+
+    updateScreenText(text = ''){
+        if(operationsSigns.includes(text)){
+            text = ` ${text} `
+        }
+        this.screenText += text;
+        screen.textContent = this.screenText;
+    },
+
+    equal(){
+        let [left,op,right] = this.screenText.split(" ");
+        left = Number(left);
+        right = Number(right);
+
+        switch(op){
+            case '+':
+                this.sum = left + right;
+                break;
+            case '-':
+                this.sum = left - right;
+                break;
+            case 'x':
+                this.sum = left * right;
+                break;
+            case '/':
+                if(right === 0){
+                    this.displayError('Cannot divide by zero.');
+                    return;
+                } 
+                this.sum = Number.parseFloat((left / right)).toFixed(2);                
+                break;
+        }
+        this.screenText = this.sum;
+        this.updateScreenText();
+    },
+
+    displayError(error){
+        this.screenText = error;
+        this.updateScreenText();
+        // Reset to default in case the user enter a number if the error
+        // is displayed.
+        this.screenText = '';
+    },
+
+    clear(){
+        this.screenText = '';
+        this.updateScreenText();
+    },
 }
 
-operations.addEventListener('click', (event) => {
-    const target = event.target;
+app.addEventListener('click', (event) => {
+    const target = event.target.id;
+    const calc = calculatorObj;
 
-    switch (target.id) {
+    switch(target){
         case 'clear':
-            clearOp(calculatorObj);
+            calc.clear();
             break;
-        case 'plus':
-            break;
-        case 'minus':
-            break;
-        case 'multiplication':
-            break;
-        case 'division':
-            break;
-        case 'equal':
+        case '=':
+            calc.equal();
             break;
         default:
-
+            calc.updateScreenText(target);
             break;
     }
 })
