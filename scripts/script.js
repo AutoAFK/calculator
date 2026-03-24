8e760b4c705cb198c97ad3995d12ab323b0433da
AutoAFK <ranhaim85@gmail.com>
2026-03-24 19:52:44 +0200

    Can only put one decmial point for each number

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 const app = document.querySelector("#app");
 const screen = document.querySelector(".screen");
+const decimalPointButton = document.querySelector("#decimalPoint")
 
 const operationsSigns = "+-x/";
 const isOpeartion = (text) => operationsSigns.includes(text);
@@ -9,28 +10,36 @@ const calculatorObj = {
     screenText: '',
     opCount: 0,
 
-    appendScreenText(text){
-        if(typeof text !== "string") return;
-        if(operationsSigns.includes(text)){
+    appendScreenText(text) {
+        if (typeof text !== "string") return;
+        if (operationsSigns.includes(text)) {
             text = ` ${text} `
             this.opCount++;
+            if (decimalPointButton.disabled) {
+                this.toggleHasDecimal();
+            }
         }
-        if(text !== ''){
+        if (text !== '') {
             this.screenText += text;
         }
         this.updateScreen();
     },
 
-    updateScreen(){
+    updateScreen() {
         screen.textContent = this.screenText;
     },
 
-    equal(){
-        let [left,op,right] = this.screenText.trim().split(" ");
-        left = Number(left);
-        right = Number(right);
+    equal() {
+        let [left = 0, op = '+', right = 0] = this.screenText.trim().split(" ");
 
-        switch(op){
+        left = !left ? 0 : left == '.' ? 0 : left;
+        op = op || '+';
+        right = !right ? 0 : right == '.' ? 0 : right;
+
+        left = Number.parseFloat(left);
+        right = Number.parseFloat(right);
+
+        switch (op) {
             case '+':
                 this.sum = left + right;
                 break;
@@ -41,20 +50,25 @@ const calculatorObj = {
                 this.sum = left * right;
                 break;
             case '/':
-                if(right === 0){
+                if (right === 0) {
                     this.displayError('Cannot divide by zero.');
                     return;
-                } 
-                this.sum = Number.parseFloat((left / right)).toPrecision(2);                
+                }
+                this.sum = Number.parseFloat((left / right)).toPrecision(2);
                 break;
         }
         this.sum = String(this.sum).trim();
         this.screenText = this.sum;
         this.opCount = 0;
         this.updateScreen();
+        if (Number.isInteger(this.sum)) {
+            decimalPointButton.disabled = false;
+        } else {
+            decimalPointButton.disabled = true;
+        }
     },
 
-    displayError(error){
+    displayError(error) {
         this.screenText = error;
         this.updateScreen();
         // Reset to default in case the user enter a number if the error
@@ -62,24 +76,34 @@ const calculatorObj = {
         this.screenText = '';
     },
 
-    hasOperation(){
+    hasOperation() {
         return this.opCount >= 1;
     },
 
-    clear(){
+    toggleHasDecimal() {
+        console.log(decimalPointButton.disabled);
+        decimalPointButton.disabled = !decimalPointButton.disabled;
+    },
+
+    clear() {
         this.screenText = '';
         this.sum = 0;
         this.opCount = 0;
         this.updateScreen();
+        decimalPointButton.disabled = false;
     },
 }
 
 app.addEventListener('click', (event) => {
-    if(event.target.tagName !== 'BUTTON') return;
-    const target = event.target.id;
+    if (event.target.tagName !== 'BUTTON') return;
+    const target = event.target;
     const calc = calculatorObj;
 
-    switch(target){
+    switch (target.id) {
+        case 'decimalPoint':
+            calc.appendScreenText(".");
+            calc.toggleHasDecimal();
+            break;
         case 'clear':
             calc.clear();
             break;
@@ -87,10 +111,10 @@ app.addEventListener('click', (event) => {
             calc.equal();
             break;
         default:
-            if(isOpeartion(target) && calc.hasOperation()){
+            if (isOpeartion(target.id) && calc.hasOperation()) {
                 calc.equal();
             }
-            calc.appendScreenText(target);
+            calc.appendScreenText(target.id);
             break;
     }
 })
